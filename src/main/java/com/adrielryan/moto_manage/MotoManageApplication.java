e7d8fb6f7acae84540d84ba1b78a2d7fca3581e9
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-08-22 18:00:50 -0300

    Estruturação do projeto e application.properties

---
+++ src/main/java/com/adrielryan/moto_manage/MotoManageApplication.java
@@ -0,0 +1,13 @@
+package com.adrielryan.moto_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoManageApplication.class, args);
+	}
+
+}
